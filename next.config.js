f565487831723a9fa1bb5db83c5a0f947d91bb17
Arthur Marconcini <arthur.m.ramos@hotmail.com>
2021-08-25 21:00:20 -0300

    feat: login status

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: true
+}
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts)x?$/]
+      },
+      use: ['@svgr/webpack']
+    })
+    return config
+  }
 }
---
+++ pages/index.jsx
@@ -0,0 +1,124 @@
+import {
+  Box,
+  Input,
+  Button,
+  Container,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  InputGroup,
+  InputLeftAddon
+} from '@chakra-ui/react'
+
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
+import { Logo } from './../components/Logo'
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Email inválido')
+    .required('Preenchimento obrigatório'),
+  password: yup.string().required('Preenchimento obrigatório'),
+  username: yup.string().required('Preenchimento obrigatório')
+})
+
+export default function Home() {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting
+  } = useFormik({
+    onSubmit: (values, form) => {
+      console.log(values)
+    },
+    validationSchema,
+    initialValues: {
+      email: '',
+      username: '',
+      password: ''
+    }
+  })
+
+  return (
+    <div>
+      <Container p={4} centerContent>
+        <Logo />
+        <Box p={4} mt={8}>
+          <Text>Crie sua agenda compartilhada</Text>
+        </Box>
+
+        <Box>
+          <FormControl id="email" p={4} isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input
+              size="lg"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {touched.email && (
+              <FormHelperText textColor="#e74c3c">
+                {errors.email}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <FormControl id="password" p={4} isRequired>
+            <FormLabel>Senha</FormLabel>
+            <Input
+              size="lg"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {touched.password && (
+              <FormHelperText textColor="#e74c3c">
+                {errors.password}
+              </FormHelperText>
+            )}
+          </FormControl>
+
+          <Box display="flex" p={4} flexDirection="row" alignItems="center">
+            <FormControl id="username" isRequired>
+              <InputGroup size="lg">
+                <InputLeftAddon children="clocker.work/" />
+                <Input
+                  size="lg"
+                  type="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </InputGroup>
+              {touched.username && (
+                <FormHelperText textColor="#e74c3c">
+                  {errors.username}
+                </FormHelperText>
+              )}
+            </FormControl>
+          </Box>
+
+          <Box p={4}>
+            <Button
+              colorScheme="blue"
+              width="100%"
+              onClick={handleSubmit}
+              isLoading={isSubmitting}
+            >
+              Entrar
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  )
+}
